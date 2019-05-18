4586cebb024179cad2de86600c5ac83481a26cf1
adhaamehab <adhaamehab7@gmail.com>
2019-05-19 01:29:45 +0200

    Release Dash app V1.0

--- app.py
+++ app.py
@@ -1,76 +1,162 @@
 # -*- coding: utf-8 -*-
 import os
 
-import numpy as np
 
-import flask
 import dash
-from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 import base64
-
+from src.model import DeepLabModel
+from src.utils import inference
+import requests
 
 # Setup the app
-app = dash.Dash(__name__, static_folder='./static/')
+app = dash.Dash(
+    __name__,
+    static_folder="./static/",
+    assets_external_path="https://fonts.googleapis.com/css?family=Cairo&display=swap",
+)
+
 server = app.server
-STATIC_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+STATIC_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "static")
 
-default_img = STATIC_PATH + '/demo.png'
-print(default_img)
-encoded_image = base64.b64encode(open(default_img, 'rb').read())
+MODEL_URL = 'https://www.dropbox.com/s/upy0a07e38243wc/ade20kmodel_may2019.gz?dl=1'
 
-app.layout = html.Div([
-    html.Div([
-        html.H1(
-            children="Semantic Segmentation Live Demo",
-            style={
-                'textAlign': 'left',
-                'color': '#b52b3b'
-            }
+
+def download_file(url):
+    local_filename = 'ade20kmodel_may2019.gz'
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(local_filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                if chunk:
+                    f.write(chunk)
+                    # f.flush()
+    return local_filename
+
+
+download_file(MODEL_URL)
+MODEL = DeepLabModel("ade20kmodel_may2019.gz")
+
+default_img = STATIC_PATH + "/demo.png"
+
+encoded_image = base64.b64encode(open(default_img, "rb").read())
+
+app.layout = html.Div(
+    [
+        html.Div(
+            [
+                html.H1(
+                    children="Semantic Segmentation Live Demo",
+                    style={"textAlign": "center", "color": "#e64659", "size": "30px"},
+                ),
+                html.P(
+                    [
+                        html.P("Image segmentation using tensorflow"),
+                        html.P(
+                            "This is model is a retrained version of deeplab based on tensorflow pre-trained model."
+                        ),
+                        html.P(
+                            "which uses Xception_65 as backbone network and Image net as backbone dataset"
+                        ),
+                        html.P(
+                            children=[
+                                "By ",
+                                html.A(
+                                    href="https://adhaamehab.me", children="Adham Ehab"
+                                ),
+                            ]
+                        ),
+                    ],
+                    style={"textAlign": "center"},
+                ),
+            ]
         ),
-        html.H3(
-            """
-            Image segmentation using tensorflow
-
-            This is a retrained model based on tensorflow pre-trained model.
-
-            uses Xception_65 as backbone network and Image net as backbone dataset
-            """,
-            style={
-                'textAlign': 'left',
-                'color': '#b52b3b'
-            }
-        )
-    ]),
-
-    html.Hr(),
-
-    html.Div([
-        dcc.Input(value='', type='text', placeholder="Enter image url"),
-        html.Button("Hit", id='predict')
-
-    ]),
-
-    html.Hr(),
-
-    html.Div([
-        html.Img(src='data:image/png;base64,{}'.format(encoded_image.decode()))
-    ]),
-
-    html.Hr(),
-
-    html.Div([
-        html.P(
-            'A detailed article about this demo and the backbone model will be published at towardsdatascience.'
+        html.Hr(),
+        html.Div(
+            [
+                dcc.Input(
+                    value="",
+                    id="input-box",
+                    type="text",
+                    placeholder="Enter image url",
+                    style={"width": "900px", "height": "40px", "align": "center"},
+                ),
+                html.Button(
+                    "Hit",
+                    id="predict",
+                    style={
+                        "width": "100px",
+                        "height": "40px",
+                        "background-color": "#e64659",
+                        "font-size": "large",
+                        "margin": "10px",
+                    },
+                ),
+            ],
+            style={"transform": "translate(20%, 5%)"},
         ),
-        html.P([
-            html.P(children=['You can also view the full source code ', html.A(href="https://github.com/adhaamehab/scene-segmentation", children="here")]),
-            html.P(children=['Or visit the author site ', html.A(href="https://adhaamehab.me", children="here")]),
+        html.Hr(),
+        html.Div(
+            [
+                html.Img(
+                    src="data:image/png;base64,{}".format(encoded_image.decode()),
+                    id="result-img",
+                )
+            ]
+        ),
+        html.Hr(),
+        html.Div(
+            [
+                html.P(
+                    "A detailed article about this demo and the backbone model will be published at towardsdatascience."
+                ),
+                html.P(
+                    [
+                        html.P(
+                            children=[
+                                "You can also view the full source code ",
+                                html.A(
+                                    href="https://github.com/adhaamehab/scene-segmentation",
+                                    children="here",
+                                ),
+                            ]
+                        ),
+                        html.P(
+                            children=[
+                                "Or visit the author site ",
+                                html.A(href="https://adhaamehab.me", children="here"),
+                            ]
+                        ),
+                    ]
+                ),
+            ]
+        ),
+    ],
+    style={"font-family": "Cairo"},
+)
 
-        ])
-    ])
-])
+
+@app.callback(
+    dash.dependencies.Output("result-img", "src"),
+    [dash.dependencies.Input("predict", "n_clicks")],
+    [dash.dependencies.State("input-box", "value")],
+)
+def predict_image(n_clicks, url):
+    if n_clicks == 0:
+        return "data:image/png;base64,{}".format(encoded_image.decode())
+    else:
+        try:
+            print(url)
+            res = "data:image/png;base64,{}".format(
+                base64.b64encode(inference(MODEL, url).read()).decode()
+            )
+            return res
+        except Exception as e:
+            print(e, url)
+            return "data:image/png;base64,{}".format(encoded_image.decode())
+
 
 if __name__ == "__main__":
+
     app.run_server(debug=True)
--- src/utils.py
+++ src/utils.py
@@ -1,40 +1,38 @@
 import numpy as np
 import requests
 
-from io import StringIO, BytesIO
-import base64
+from io import BytesIO
 
 from matplotlib import gridspec
 from matplotlib import pyplot as plt
-import numpy as np
 from PIL import Image
 
-import tensorflow as tf
 
+"""Adobted from github"""
 
-'''Adobted from github'''
 
 def create_pascal_label_colormap():
-  """Creates a label colormap used in PASCAL VOC segmentation benchmark.
+    """Creates a label colormap used in PASCAL VOC segmentation benchmark.
 
   Returns:
     A Colormap for visualizing segmentation results.
   """
-  colormap = np.zeros((256, 3), dtype=int)
-  ind = np.arange(256, dtype=int)
+    colormap = np.zeros((256, 3), dtype=int)
+    ind = np.arange(256, dtype=int)
 
-  for shift in reversed(range(8)):
-    for channel in range(3):
-      colormap[:, channel] |= ((ind >> channel) & 1) << shift
-    ind >>= 3
+    for shift in reversed(range(8)):
+        for channel in range(3):
+            colormap[:, channel] |= ((ind >> channel) & 1) << shift
+        ind >>= 3
 
-  return colormap
+    return colormap
 
 
-'''Adobted from github'''
+"""Adobted from github"""
+
 
 def label_to_color_image(label):
-  """Adds color defined by the dataset colormap to the label.
+    """Adds color defined by the dataset colormap to the label.
 
   Args:
     label: A 2D array with integer type, storing the segmentation label.
@@ -48,15 +46,15 @@ def label_to_color_image(label):
     ValueError: If label is not of rank 2 or its value is larger than color
       map maximum entry.
   """
-  if label.ndim != 2:
-    raise ValueError('Expect 2-D input label')
+    if label.ndim != 2:
+        raise ValueError("Expect 2-D input label")
 
-  colormap = create_pascal_label_colormap()
+    colormap = create_pascal_label_colormap()
 
-  if np.max(label) >= len(colormap):
-    raise ValueError('label value too large.')
+    if np.max(label) >= len(colormap):
+        raise ValueError("label value too large.")
 
-  return colormap[label]
+    return colormap[label]
 
 
 def inference(model, url):
@@ -65,10 +63,10 @@ def inference(model, url):
         img_ = requests.get(url).content
         original_im = Image.open(BytesIO(img_))
     except IOError:
-        print('Invalid image url' + url)
+        print("Invalid image url" + url)
         return
 
-    print('Predicting ....')
+    print("Predicting ....")
     image, seg_map = model.run(original_im)
 
     plt.figure(figsize=(15, 5))
@@ -76,38 +74,191 @@ def inference(model, url):
 
     plt.subplot(grid_spec[0])
     plt.imshow(image)
-    plt.axis('off')
-    plt.title('input image')
+    plt.axis("off")
+    plt.title("input image")
 
     plt.subplot(grid_spec[1])
     seg_image = label_to_color_image(seg_map).astype(np.uint8)
     plt.imshow(seg_image)
-    plt.axis('off')
-    plt.title('segmentation map')
+    plt.axis("off")
+    plt.title("segmentation map")
 
     plt.subplot(grid_spec[2])
     plt.imshow(image)
     plt.imshow(seg_image, alpha=0.7)
-    plt.axis('off')
-    plt.title('segmentation overlay')
-
+    plt.axis("off")
+    plt.title("segmentation overlay")
     unique_labels = np.unique(seg_map)
     ax = plt.subplot(grid_spec[3])
     plt.imshow(
-        CM[unique_labels].astype(np.uint8), interpolation='nearest') # adobted from tensorflow - model zoo
+        CM[unique_labels].astype(np.uint8), interpolation="nearest"
+    )  # adobted from tensorflow - model zoo
     ax.yaxis.tick_right()
     plt.yticks(range(len(unique_labels)), NAMES[unique_labels])
     plt.xticks([], [])
     ax.tick_params(width=0.0)
-    plt.grid('off')
+    plt.grid("off")
+
     img = BytesIO()
-    plt.savefig(img, format='png')
+    plt.savefig(img, format="png")
     img.seek(0)
     return img
 
-NAMES = np.asarray([
-'airplane, aeroplane, plane','animal, animate being, beast, brute, creature, fauna','apparel, wearing apparel, dress, clothes','arcade machine','armchair','ashcan, trash can, garbage can, wastebin, ash bin, ash-bin, ashbin, dustbin, trash barrel, trash bin','awning, sunshade, sunblind','background','bag','ball','bannister, banister, balustrade, balusters, handrail','bar','barrel, cask','base, pedestal, stand','basket, handbasket','bathtub, bathing tub, bath, tub','bed ','bench','bicycle, bike, wheel, cycle ','blanket, cover','blind, screen','boat','book','bookcase','booth, cubicle, stall, kiosk','bottle','box','bridge, span','buffet, counter, sideboard','building, edifice','bulletin board, notice board','bus, autobus, coach, charabanc, double-decker, jitney, motorbus, motorcoach, omnibus, passenger vehicle','cabinet','canopy','car, auto, automobile, machine, motorcar','case, display case, showcase, vitrine','ceiling','chair','chandelier, pendant, pendent','chest of drawers, chest, bureau, dresser','clock','coffee table, cocktail table','column, pillar','computer, computing machine, computing device, data processor, electronic computer, information processing system','conveyer belt, conveyor belt, conveyer, conveyor, transporter','counter','countertop','cradle','crt screen','curtain, drape, drapery, mantle, pall','cushion','desk','dirt track','dishwasher, dish washer, dishwashing machine','door, double door','earth, ground','escalator, moving staircase, moving stairway','fan','fence, fencing','field','fireplace, hearth, open fireplace','flag''floor, flooring','flower','food, solid food','fountain','glass, drinking glass','grandstand, covered stand','grass','hill','hood, exhaust hood','house','hovel, hut, hutch, shack, shanty','kitchen island','lake','lamp','land, ground, soil','light, light source','microwave, microwave oven','minibike, motorbike','mirror','monitor, monitoring device','mountain, mount','ottoman, pouf, pouffe, puff, hassock','oven','painting, picture','palm, palm tree','path','person, individual, someone, somebody, mortal, soul','pier, wharf, wharfage, dock','pillow','plant, flora, plant life','plate','plaything, toy','pole','pool table, billiard table, snooker table','poster, posting, placard, notice, bill, card','pot, flowerpot','radiator','railing, rail','refrigerator, icebox','river','road, route','rock, stone','rug, carpet, carpeting','runway','sand','sconce','screen door, screen','screen, silver screen, projection screen','sculpture','sea','seat','shelf','ship','shower','sidewalk, pavement','signboard, sign','sink','sky','skyscraper','sofa, couch, lounge','stage','stairs, steps','stairway, staircase','step, stair','stool','stove, kitchen stove, range, kitchen range, cooking stove','streetlight, street lamp','swimming pool, swimming bath, natatorium','swivel chair','table','tank, storage tank','television receiver, television, television set, tv, tv set, idiot box, boob tube, telly, goggle box','tent, collapsible shelter','toilet, can, commode, crapper, pot, potty, stool, throne','towel','tower','trade name, brand name, brand, marque','traffic light, traffic signal, stoplight','tray','tree','truck, motortruck','van','vase','wall','wardrobe, closet, press','washer, automatic washer, washing machine','water','waterfall, falls','windowpane, window ',
-])
+
+NAMES = np.asarray(
+    [
+        "airplane, aeroplane, plane",
+        "animal, animate being, beast, brute, creature, fauna",
+        "apparel, wearing apparel, dress, clothes",
+        "arcade machine",
+        "armchair",
+        "ashcan, trash can, garbage can, wastebin, ash bin, ash-bin, ashbin, dustbin, trash barrel, trash bin",
+        "awning, sunshade, sunblind",
+        "background",
+        "bag",
+        "ball",
+        "bannister, banister, balustrade, balusters, handrail",
+        "bar",
+        "barrel, cask",
+        "base, pedestal, stand",
+        "basket, handbasket",
+        "bathtub, bathing tub, bath, tub",
+        "bed ",
+        "bench",
+        "bicycle, bike, wheel, cycle ",
+        "blanket, cover",
+        "blind, screen",
+        "boat",
+        "book",
+        "bookcase",
+        "booth, cubicle, stall, kiosk",
+        "bottle",
+        "box",
+        "bridge, span",
+        "buffet, counter, sideboard",
+        "building, edifice",
+        "bulletin board, notice board",
+        "bus, autobus, coach, charabanc, double-decker, jitney, motorbus, motorcoach, omnibus, passenger vehicle",
+        "cabinet",
+        "canopy",
+        "car, auto, automobile, machine, motorcar",
+        "case, display case, showcase, vitrine",
+        "ceiling",
+        "chair",
+        "chandelier, pendant, pendent",
+        "chest of drawers, chest, bureau, dresser",
+        "clock",
+        "coffee table, cocktail table",
+        "column, pillar",
+        "computer, computing machine, computing device, data processor, electronic computer, information processing system",
+        "conveyer belt, conveyor belt, conveyer, conveyor, transporter",
+        "counter",
+        "countertop",
+        "cradle",
+        "crt screen",
+        "curtain, drape, drapery, mantle, pall",
+        "cushion",
+        "desk",
+        "dirt track",
+        "dishwasher, dish washer, dishwashing machine",
+        "door, double door",
+        "earth, ground",
+        "escalator, moving staircase, moving stairway",
+        "fan",
+        "fence, fencing",
+        "field",
+        "fireplace, hearth, open fireplace",
+        "flag" "floor, flooring",
+        "flower",
+        "food, solid food",
+        "fountain",
+        "glass, drinking glass",
+        "grandstand, covered stand",
+        "grass",
+        "hill",
+        "hood, exhaust hood",
+        "house",
+        "hovel, hut, hutch, shack, shanty",
+        "kitchen island",
+        "lake",
+        "lamp",
+        "land, ground, soil",
+        "light, light source",
+        "microwave, microwave oven",
+        "minibike, motorbike",
+        "mirror",
+        "monitor, monitoring device",
+        "mountain, mount",
+        "ottoman, pouf, pouffe, puff, hassock",
+        "oven",
+        "painting, picture",
+        "palm, palm tree",
+        "path",
+        "person, individual, someone, somebody, mortal, soul",
+        "pier, wharf, wharfage, dock",
+        "pillow",
+        "plant, flora, plant life",
+        "plate",
+        "plaything, toy",
+        "pole",
+        "pool table, billiard table, snooker table",
+        "poster, posting, placard, notice, bill, card",
+        "pot, flowerpot",
+        "radiator",
+        "railing, rail",
+        "refrigerator, icebox",
+        "river",
+        "road, route",
+        "rock, stone",
+        "rug, carpet, carpeting",
+        "runway",
+        "sand",
+        "sconce",
+        "screen door, screen",
+        "screen, silver screen, projection screen",
+        "sculpture",
+        "sea",
+        "seat",
+        "shelf",
+        "ship",
+        "shower",
+        "sidewalk, pavement",
+        "signboard, sign",
+        "sink",
+        "sky",
+        "skyscraper",
+        "sofa, couch, lounge",
+        "stage",
+        "stairs, steps",
+        "stairway, staircase",
+        "step, stair",
+        "stool",
+        "stove, kitchen stove, range, kitchen range, cooking stove",
+        "streetlight, street lamp",
+        "swimming pool, swimming bath, natatorium",
+        "swivel chair",
+        "table",
+        "tank, storage tank",
+        "television receiver, television, television set, tv, tv set, idiot box, boob tube, telly, goggle box",
+        "tent, collapsible shelter",
+        "toilet, can, commode, crapper, pot, potty, stool, throne",
+        "towel",
+        "tower",
+        "trade name, brand name, brand, marque",
+        "traffic light, traffic signal, stoplight",
+        "tray",
+        "tree",
+        "truck, motortruck",
+        "van",
+        "vase",
+        "wall",
+        "wardrobe, closet, press",
+        "washer, automatic washer, washing machine",
+        "water",
+        "waterfall, falls",
+        "windowpane, window ",
+    ]
+)
 
 LM = np.arange(len(NAMES)).reshape(len(NAMES), 1)
 CM = label_to_color_image(LM)
